8befb6b94c65b5c999d0d95d3cfb98ca75a169b5
BugHunters21pc <116105892+BugHunters21pc@users.noreply.github.com>
2023-03-30 12:52:49 +0530

    Update cat.c

--- cat.c
+++ cat.c
@@ -1,25 +1,26 @@
-#include<sys/stat.h>
-#include<sys/types.h>
 #include<stdio.h>
+#include<sys/stat.h>
 #include<unistd.h>
+#include<sys/types.h>
 #include<fcntl.h>
-int main(int argc,char *argv[]){
-        int fd1,fd2,i;
-        char buff[2];
-        char *a1=argv[1],*a2 = argv[2];
-        fd1 = open(a1,O_RDONLY,0777);
-        fd2 = creat(a2,0777);
-        if(fd1==-1){
-                printf("\nError in openning a File.");
-        }
-        else{
-        while(i=read(fd1,&buff[0],1)){
-                write(fd2,&buff[0],1);
-        }
-        close(fd1);
-        close(fd2);
-        remove(a1);
-        }
-        return 0;
 
+int main(int argc,char *argv[])
+{
+    int fd1,i;
+    char buff[2];
+
+  for(int j=1;j<argc;j++){
+    fd1 = open(argv[j],O_RDONLY,0777);
+    if(fd1==-1)
+        printf("Error.");
+    else
+    {
+        while(i=read(fd1,&buff[0],1)){
+            printf("%c",buff[0]);
+        }
+    }
+    close(fd1);
+ }
+
+    return 0;
 }
